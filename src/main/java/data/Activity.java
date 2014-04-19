b4f85c406a2715ae625ac0830350e14edf73d351
Brett Harper <jbharper86@gmail.com>
2014-04-18 21:46:50 -0400

    More data changes

--- src/main/java/data/Activity.java
+++ src/main/java/data/Activity.java
@@ -9,56 +9,97 @@ public class Activity implements Serializable {
 	public static final long serialVersionUID = 84579384573489L;
 
 	private DateTime startTime;
-	private DateTime finishTime;
-	private int arriveMileage;
-	private int leaveMileage;
+	private DateTime stopTime;
+	private String patientName;
+	private String medicalRecordNumber;
 	private ServiceCode type;
+
+	private DateTime travelStart;
+	private DateTime travelStop;
+	private int startMileage;
+	private int stopMileage;
 
 	public DateTime getStartTime() {
 		return startTime;
 	}
 
-	public DateTime getFinishTime() {
-		return finishTime;
+	public DateTime getStopTime() {
+		return stopTime;
 	}
 
-	public int getArriveMileage() {
-		return arriveMileage;
+	public DateTime getTravelStart() {
+		return travelStart;
 	}
 
-	public int getLeaveMileage() {
-		return leaveMileage;
+	public DateTime getTravelStop() {
+		return travelStop;
+	}
+
+	public int getStartMileage() {
+		return startMileage;
+	}
+
+	public int getStopMileage() {
+		return stopMileage;
 	}
 
 	public ServiceCode getType() {
 		return type;
 	}
 
+	public String getPatientName() {
+		return patientName;
+	}
+
+	public String getMedicalRecordNumber() {
+		return medicalRecordNumber;
+	}
+
 	public int getTotalMiles() {
-		return getArriveMileage() - getLeaveMileage();
+		return getStopMileage() - getStartMileage();
 	}
 
 	public Duration getTotalTime() {
-		return new Duration(getStartTime(), getFinishTime());
+		return new Duration(getStartTime(), getStopTime());
+	}
+
+	public Duration getTotalTravelTime() {
+		return new Duration(getTravelStart(), getTravelStop());
 	}
 
 	public void setStartTime(final DateTime startTime) {
 		this.startTime = startTime;
 	}
 
-	public void setFinishTime(final DateTime finishTime) {
-		this.finishTime = finishTime;
+	public void setFinishTime(final DateTime stopTime) {
+		this.stopTime = stopTime;
 	}
 
-	public void setArriveMileage(final int arriveMileage) {
-		this.arriveMileage = arriveMileage;
+	public void setTravelStart(final DateTime travelStart) {
+		this.travelStart = travelStart;
 	}
 
-	public void setLeaveMileage(final int leaveMileage) {
-		this.leaveMileage = leaveMileage;
+	public void setTravelStop(final DateTime travelStop) {
+		this.travelStop = travelStop;
+	}
+
+	public void setStartMileage(final int startMileage) {
+		this.startMileage = startMileage;
+	}
+
+	public void setStopMileage(final int stopMileage) {
+		this.stopMileage = stopMileage;
 	}
 
 	public void setType(final ServiceCode type) {
 		this.type = type;
+	}
+
+	public void setPatientName(final String patientName) {
+		this.patientName = patientName;
+	}
+
+	public void setMedicalRecordNumber(final String medicalRecordNumber) {
+		this.medicalRecordNumber = medicalRecordNumber;
 	}
 }
--- src/main/java/data/ServiceCode.java
+++ src/main/java/data/ServiceCode.java
@@ -4,36 +4,38 @@ import java.io.Serializable;
 
 public enum ServiceCode implements Serializable {
 
-	_900CH("Chaplain Charting"),
-	_900BRV("Bereavement Charting"),
-	_901("Travel"),
-	_825IDT("IDT Meeting"),
-	_825SU("Standup Meeting"),
-	_825CN("Community Networking"),
-	_825IS("In-Service"),
-	_700CH("Chaplain Follow-up visit"),
-	_700CH1("Chaplain After-hours visit (<2 hours)"),
-	_700CH2("Chaplain After-hours visit (>2 hours)"),
-	_700ACH("Attempted Chaplain visit"),
-	_700A("Initial Spiritual Assessment"),
-	_700A1("After Hours - Initial Spiritual Assessment (<2 hours)"),
-	_700A2("After Hours - Initial Spiritual Assessment (>2 hours)"),
-	_700IBRV("Initial Bereavement Assessment"),
-	_700BRV("Bereavement Follow-up visit"),
-	_700BRV1("After Hours Bereavement Follow-up visit (<2 hours)"),
-	_700BRV2("After Hours Bereavement Follow-up visit (>2 hours)"),
-	_700BRPH("Bereavement Phone Call"),
-	_700ABRV("Attempted Bereavement Follow-up visit"),
-	_700REC("Chaplain Recert visit"),
-	_700PRN("Chaplain PRN visit"),
-	_700MS("Chaplain Funeral/Memorial Service");
+	_900CH("Chaplain Charting", false),
+	_900BRV("Bereavement Charting", false),
+	_901("Travel", false),
+	_825IDT("IDT Meeting", false),
+	_825SU("Standup Meeting", false),
+	_825CN("Community Networking", false),
+	_825IS("In-Service", false),
+	_700CH("Chaplain Follow-up visit", true),
+	_700CH1("Chaplain After-hours visit (<2 hours)", true),
+	_700CH2("Chaplain After-hours visit (>2 hours)", true),
+	_700ACH("Attempted Chaplain visit", true),
+	_700A("Initial Spiritual Assessment", true),
+	_700A1("After Hours - Initial Spiritual Assessment (<2 hours)", true),
+	_700A2("After Hours - Initial Spiritual Assessment (>2 hours)", true),
+	_700IBRV("Initial Bereavement Assessment", true),
+	_700BRV("Bereavement Follow-up visit", true),
+	_700BRV1("After Hours Bereavement Follow-up visit (<2 hours)", true),
+	_700BRV2("After Hours Bereavement Follow-up visit (>2 hours)", true),
+	_700BRPH("Bereavement Phone Call", false),
+	_700ABRV("Attempted Bereavement Follow-up visit", true),
+	_700REC("Chaplain Recert visit", true),
+	_700PRN("Chaplain PRN visit", true),
+	_700MS("Chaplain Funeral/Memorial Service", true);
 
 	public static final long serialVersionUID = 3294873857389593L;
 
 	private final String description;
+	private final boolean showPatientInfo;
 
-	private ServiceCode(final String description) {
+	private ServiceCode(final String description, final boolean showPatientInfo) {
 		this.description = description;
+		this.showPatientInfo = showPatientInfo;
 	}
 
 	public String code() {
@@ -42,5 +44,9 @@ public enum ServiceCode implements Serializable {
 
 	public String description() {
 		return description;
+	}
+
+	public boolean showPatientInfo() {
+		return showPatientInfo;
 	}
 }
